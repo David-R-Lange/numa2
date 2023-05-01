b2924a3cc353971f61131d970b2a1a0853ed39f5
David R. Lange <118757109+David-R-Lange@users.noreply.github.com>
2023-05-01 21:49:04 +0200

    Add files via upload

--- solve_LAP.m
+++ solve_LAP.m
@@ -17,19 +17,19 @@ function x = solve_LAP( A, a, C, c)
   
   [QcR, tau_c] = qr_Householder(C);
   
-  R = triu(QcR);
-  
   % Bestimme Loesungskomponente x1 mit R (inklusive trafo von x)
-  x1 = trisolve(0, R, c)';
+  x1 = trisolve(1, triu(QcR)', c);
    
   % Transformation von A mit [A1 A2] = AQc
-  a0 = a - mult_QT(QcR, tau_c, x1);
+  
+  a0 = a - mult_QT(QcR'*A', tau_c, x1);
   
   % Bestimme QR-Zerlegung zu A2 mit A2 = QaR mit R = [R1 0]T
+  
   [QaR, tau_a] = qr_Householder(A2);
   
   % Transformation von a0: (a1 a2)' = QaTa0
-  [a1 a2] = mult_QT(QaR, tau, a0);
+  a1_2 = mult_QT(QaR, tau, a0);
   
   % Bestimme Loesungskomponente x_a: R1x_a = a1
   x_a = trisolve(0, triu(QaR), a1);
