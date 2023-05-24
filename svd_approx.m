b61ae98a1a0df472ed11401a3e566f675b3d837c
David-R-Lange <renedavidbrueggemann@gmail.com>
2023-05-24 19:59:35 +0200

    changed Matrix Mult. and added comments

--- svd_approx.m
+++ svd_approx.m
@@ -13,12 +13,12 @@ function C = svd_approx(A, nu)
         error("\nFehler. Der Rang der TruncSVD ist groesser als der Rang von der gegebenen Matrix.\n")
     endif
 
-    C = zeros(m,n);
+    C = zeros(m,n);                     # Null-Matrix mit dim. mxn fuer addition 
 
-    [U, S, V] = svd(A);
+    [U, S, V] = svd(A);                 # Interne SVD nutzen um U, S, V zu bekommen
 
     for j = 1:nu
-        C += U(:,j) * S(j,j) * V(:,j)';
+      C += S(j,j) * U(:,j) * V(:,j)';   # Implizite Matrix-Mult mit U(:,j) = mx1, S(j,j) = 1x1, V(:,j)' = 1xm
     endfor
 
 endfunction
