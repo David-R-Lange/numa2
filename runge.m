d60f9d07d194fedb2c3fd434730abeed35a8bd0c
David-R-Lange <renedavidbrueggemann@gmail.com>
2023-06-13 19:03:00 +0200

    Implemented Runge's method

---
+++ runge.m
@@ -0,0 +1,22 @@
+%%Funktion zum Berechnen einer approximativen Loesung eines AWP mit dem Runge-Verfahren
+%
+% Input:    -   f		:	Gegebene Funktion des AWP
+%           -   zerl	:	Zerlegung der Zeitvariable
+%	        -	y_0		:	Anfangswert
+%
+% Output:   -   vals	:	nxn Matrix mit den Spalten als Loesungen vom AWP
+%
+function vals = runge(f, zerl, y_0)
+    [~,n] = size(zerl);
+
+    vals = zeros(n,n);
+    vals(:,1) = y_0;
+
+    for i = 1:n-1
+        tau = zerl(i+1) - zerl(i);  % Definition von tau, damit Inkrementsfunktion besser lesbar
+        
+        vals(:,i+1) =  vals(i,i) + tau * (f(zerl(i) + tau * 0.5,vals(i,i)) +
+                           tau * 0.5 * f(zerl(i), vals(i,i))
+                        );
+    end
+end
