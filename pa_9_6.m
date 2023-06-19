20e8ab8ecae9d00720549977684bbfee128282ef
David-R-Lange <renedavidbrueggemann@gmail.com>
2023-06-19 13:32:04 +0200

    started implementing assignment 9.6

---
+++ pa_9_6.m
@@ -0,0 +1,20 @@
+% Programmieraufgabe 9.6
+% Liste der Gruppenmitglieder (Name, Matrikelnummer)
+%   1.  Rene-David Brüggemann, 10040204
+%   2.  Jean-Luc Brüggemann, 10048805
+%   3.  Kilian Hartmann, 10031802
+%
+% Programmiersprache: Octave
+% Version:            version 7.3.0
+%
+% Liste der eingesendeten Dateien:
+%   1.  runge_kutta.m
+%
+% Housekeeping
+clc;
+clear all;
+close all;
+tic;    % start clock
+
+% Anfang des Skripte
+
---
+++ runge_kutta.m
@@ -0,0 +1,11 @@
+%%Funktion zum Berechnen einer approximativen Loesung einer ODE mit dem allgemeinen expliziten s-stufigen Runge-Kutta-Verfahren 
+%
+% Input:    -   f		:	Gegebene Funktion des AWP
+%           -   zerl	:	Zerlegung der Zeitvariable
+%	        -	y_0		:	Anfangswert
+%
+% Output:   -   vals	:	nxn Matrix mit den Spalten als Loesungen vom AWP
+%
+function vals = runge_kutta(A, b, f, t0, tend, y0, n)
+
+end
